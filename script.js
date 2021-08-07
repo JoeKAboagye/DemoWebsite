c55ddfa8b7d2c87f8f880e4ab4b031a310f24049
JoeKAboagye <josephkwabenaaboagye@gmail.com>
2021-08-07 18:17:01 +0000

    Additions and Updates

---
+++ script.js
@@ -0,0 +1,9 @@
+// Log-in  time on my website
+let d=new Date(); 
+let weekday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday", "Saturday"); 
+let monthname=new Array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"); 
+document.write(weekday[d.getDay()] + " "); 
+document.write(d.getDate() + ", "); 
+document.write(monthname[d.getMonth()] + " "); 
+document.write(d.getFullYear() + ", ");
+document.write(d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds());
