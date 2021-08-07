f127f717ac6ac0937282ba05eba5b802c037b0d3
JoeKAboagye <josephkwabenaaboagye@gmail.com>
2021-08-07 18:19:00 +0000

    Additions and Updates

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Log-in  time on my website
+// Log-in time on my website
 let d=new Date(); 
 let weekday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday", "Saturday"); 
 let monthname=new Array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"); 
